5fa07c964ad214088fdab607ad1e4b31cdf82514
Yesenny Fernandez <yesennyfakeemail@gmail.com>
2022-05-19 15:48:05 -0400

    s

--- Geography/main.js
+++ Geography/main.js
@@ -2,7 +2,7 @@ const map = L.map ('map', {
     center: [40.789433	-73.947589],
     Zoom: 16,
     scrollWheelZoom: true
-})
+});
 const basemapStamenTerrain = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/terrain/{z}/{x}/{y}{r}.{ext}', {
 	attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 	subdomains: 'abcd',
