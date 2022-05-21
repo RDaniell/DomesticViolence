2b1deeb4977e04efa252d516821f98ab6e9f396a
Yesenny Fernandez <yesennyfakeemail@gmail.com>
2022-05-20 23:13:59 -0400

    S

--- distributions/main.js
+++ distributions/main.js
@@ -128,12 +128,94 @@ function draw() {
         .attr("cy", d => yScale(d.TotalComplaints))
         .attr("fill", d => colorScale(d.Precinct)),
 
+        //tooltip
+// tooltip = d3.select("body")
+// .append("div")
+// .attr("class","tooltip")
+// .style("z-index", "10")
+// .style("position","absolute")
+// .style("visibility","hidden")
+// .text("tooltip")
+
+// svg.selectAll(".circle-point")
+// .data(filteredData)
+// .join("circle")
+// .attr("class", "circle-point")
+// .attr("r", "4")
+// .attr("cx", d => xScale(d.Precinct))
+// .attr("cy", d => yScale(TotalComplaints))
+// .attr("fill", "blue")
+// .attr("opacity", 0.5)
+// .on("mouseover", function(event,d,i){
+// return tooltip
+// .html(`<div>TotalComplaints: ${d.TotalComplaints}`)
+// .style("visibility", "visible");
+// .on("mouseout", function(){
+//   return tooltip.style("visibility", "hidden")
+
+// }
+// )
+
       // + HANDLE UPDATE SELECTION
       update => update,
-
       // + HANDLE EXIT SELECTION
       exit => exit
         .remove()
       )
     );
       }
+
+
+  //   //ME TESTING TOOLTIPS
+  //   voar tooltip = d3.select("#my_dataviz")
+  //   .append("div")
+  //   .style("opacity", 0)
+  //   .attr("class", "tooltip")
+  //   .style("background-color", "white")
+  //   .style("border", "solid")
+  //   .style("border-width", "1px")
+  //   .style("border-radius", "5px")
+  //   .style("padding", "10px")
+
+
+
+  // // A function that change this tooltip when the user hover a point.
+  // // Its opacity is set to 1: we can now see it. Plus it set the text and position of tooltip depending on the datapoint (d)
+  // var mouseover = function(d) {
+  //   tooltip
+  //     .style("opacity", 1)
+  // }
+
+  // var mousemove = function(d) {
+  //   tooltip
+  //     .html("The exact value of<br>the Ground Living area is: " + d.GrLivArea)
+  //     .style("left", (d3.mouse(this)[0]+90) + "px") // It is important to put the +90: other wise the tooltip is exactly where the point is an it creates a weird effect
+  //     .style("top", (d3.mouse(this)[1]) + "px")
+  // }
+
+  // // A function that change this tooltip when the leaves a point: just need to set opacity to 0 again
+  // var mouseleave = function(d) {
+  //   tooltip
+  //     .transition()
+  //     .duration(200)
+  //     .style("opacity", 0)
+  // }
+
+  // // Add dots
+  // svg.append('g')
+  //   .selectAll("dot")
+  //   .data(data.filter(function(d,i){return i<50})) // the .filter part is just to keep a few dots on the chart, not all of them
+  //   .enter()
+  //   .append("circle")
+  //     .attr("cx", function (d) { return x(d.GrLivArea); } )
+  //     .attr("cy", function (d) { return y(d.SalePrice); } )
+  //     .attr("r", 7)
+  //     .style("fill", "#69b3a2")
+  //     .style("opacity", 0.3)
+  //     .style("stroke", "white")
+  //   .on("mouseover", mouseover )
+  //   .on("mousemove", mousemove )
+  //   .on("mouseleave", mouseleave )
+    
+  //   ;
+  //     }
