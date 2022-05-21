8a6b829aa885a05f5445641b7364f2b8472c8376
Yesenny Fernandez <yesennyfakeemail@gmail.com>
2022-05-20 20:25:57 -0400

    s

--- Geography/main.js
+++ Geography/main.js
@@ -3,6 +3,15 @@ const map = L.map('map', {
     zoom: 16,
     scrollWheelzoom: true
 });
+//const basemapStamenTerrain = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/terrain/{z}/{x}/{y}{r}.{ext}', {
+//	attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+	//subdomains: 'abcd',
+	//minZoom: 0,
+	//maxZoom: 18,
+	//ext: 'png'
+//})
+    //.addTo(map);
+
     const Stamen_TonerLite = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}{r}.{ext}', {
         attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
         subdomains: 'abcd',
@@ -12,27 +21,17 @@ const map = L.map('map', {
 })
     .addTo(map);
 
-
-    $.get('../data/ENGBV_Resource_Directory.csv', function(
-        csvString) {
-            data1 = Papa.parse(
-                csvString, {header: true, dynamicTyping: true}).
-               data.filter(
-                   function(row) {return row.Shift ==="FIRSTLANGUAGE"}
-                    )
-                data2 = Papa.parse(csvString, {header: true,
-               dynamicTyping: true}).data.filter(
-                    function(row) {return row.Shift === "SECONDLANGUAGE"}
-                );
-
-
+    //var Stadia_AlidadeSmooth = L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png', {
+	//maxzoom: 20,
+	//attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
+    //})
+    //.addTo(map);
 
     $.get('../data/ENGBV_Resource_Directory.csv', function(
         csvString) {
 const data = Papa.parse(
     csvString, {header: true, dynamicTyping: true}).data;
 console.log(data)
-
 for (var i in data) {
     var row = data[i];
 
@@ -51,6 +50,10 @@ fillOpacity:0.6
 .bindPopup(popupContent);
 
 marker.addTo(map);
+
 }
-}
+
+legend.addOverlay(layer2,"LanguageServices")
+        }
     );
+    
